8a5473b2e011be40ce78fa0bb63ba33f838ae24f
songtao <songtao@mit.edu>
2022-03-06 15:06:34 -0500

    fix wrong module names

--- code/turningLaneExtraction/train.py
+++ code/turningLaneExtraction/train.py
@@ -129,7 +129,7 @@ class Train(TrainingFramework):
 
 if __name__ == "__main__":
 	#trainer = Train(sys.argv[1])
-	trainer = Train(sys.argv[1])
+	trainer = Train()
 	epochsisze = trainer.epochsize
 
 	config = {}
